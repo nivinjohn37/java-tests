d8f5fdcb573187861e9dd9f19aa0bcabb14fe745
“Nivin <nivin.john@oracle.com>
2024-04-24 21:35:37 +0530

    First Test

--- 1.00-starting-project/src/test/java/com.luv2code.junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com.luv2code.junitdemo/DemoUtilsTest.java
@@ -1,15 +1,28 @@
 package com.luv2code.junitdemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class DemoUtilsTest {
+    DemoUtils demoUtils;
 
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("Before Each executes before each test method");
+    }
 
     @Test
     void addTest() {
-        DemoUtils demoUtils = new DemoUtils();
         assertEquals(4, demoUtils.add(1, 3), "1+3 = 4");
+    }
+
+    @Test
+    void testObjectNull() {
+        String test = null;
+        assertNull(demoUtils.checkNull(null));
     }
 }
