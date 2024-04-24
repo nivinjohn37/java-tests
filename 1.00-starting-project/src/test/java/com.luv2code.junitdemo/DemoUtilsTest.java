0d647eb33c31e13bfb8be2709863b579b556535b
“Nivin <nivin.john@oracle.com>
2024-04-24 21:37:23 +0530

    First Test

--- 1.00-starting-project/src/test/java/com.luv2code.junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com.luv2code.junitdemo/DemoUtilsTest.java
@@ -15,13 +15,21 @@ class DemoUtilsTest {
         System.out.println("Before Each executes before each test method");
     }
 
+    @BeforeEach
+    void setupAfterEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("After Each executes after each test method");
+    }
+
     @Test
-    void addTest() {
+    void testAdd() {
+        System.out.println("Test Add");
         assertEquals(4, demoUtils.add(1, 3), "1+3 = 4");
     }
 
     @Test
     void testObjectNull() {
+        System.out.println("Test Null");
         String test = null;
         assertNull(demoUtils.checkNull(null));
     }
