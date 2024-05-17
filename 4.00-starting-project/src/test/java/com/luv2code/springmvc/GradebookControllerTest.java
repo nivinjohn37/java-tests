fbf1293f309b7cc865bd906cf64ae82e5fafeeff
nivinjohn <getnivin@gmail.com>
2024-05-17 23:21:42 +0530

    mvc Rest

--- 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -1,7 +1,14 @@
 package com.luv2code.springmvc;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource("/appl")
+import javax.transaction.Transactional;
+
+@TestPropertySource("/application-test.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
 public class GradebookControllerTest {
 }
