40392d6d2783a19b6462817530f8d12ddb7602dd
nivinjohn <getnivin@gmail.com>
2024-05-18 13:19:01 +0530

    rest api tests - 02

--- 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -1,14 +1,180 @@
 package com.luv2code.springmvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.HistoryGradesDao;
+import com.luv2code.springmvc.repository.MathGradesDao;
+import com.luv2code.springmvc.repository.ScienceGradesDao;
+import com.luv2code.springmvc.repository.StudentDao;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @TestPropertySource("/application-test.properties")
 @AutoConfigureMockMvc
 @SpringBootTest
 @Transactional
 public class GradebookControllerTest {
+
+    private static MockHttpServletRequest request;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Mock
+    StudentAndGradeService studentAndGradeService;
+
+    @Autowired
+    private StudentAndGradeService studentService;
+
+    @Autowired
+    private StudentDao studentDao;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private MathGradesDao mathGradeDao;
+
+    @Autowired
+    private ScienceGradesDao scienceGradeDao;
+
+    @Autowired
+    private HistoryGradesDao historyGradeDao;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CollegeStudent collegeStudent;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Value("${sql.script.create.student}")
+    private String sqlAddStudent;
+
+    @Value("${sql.script.create.math.grade}")
+    private String sqlAddMathStudent;
+
+    @Value("${sql.script.create.science.grade}")
+    private String sqlAddScienceGrade;
+
+    @Value("${sql.script.create.history.grade}")
+    private String sqlAddHistoryGrade;
+
+    @Value("${sql.script.delete.student}")
+    private String sqlDeleteStudent;
+
+    @Value("${sql.script.delete.math.grade}")
+    private String sqlDeleteMathGrade;
+
+    @Value("${sql.script.delete.science.grade}")
+    private String sqlDeleteScienceGrade;
+
+    @Value("${sql.script.delete.history.grade}")
+    private String sqlDeleteHistoryGrade;
+
+    public static final MediaType APPLICATION_JSON_UTF8 = MediaType.APPLICATION_JSON;
+
+    @BeforeAll
+    public static void beforeAll() {
+        request = new MockHttpServletRequest();
+        request.setParameter("firstName", "one");
+        request.setParameter("lastName", "test");
+        request.setParameter("emailAddress", "one.test@gmail.com");
+    }
+
+    @BeforeEach
+    public void setUpDatabase() {
+        jdbcTemplate.execute(sqlAddStudent);
+        jdbcTemplate.execute(sqlAddMathStudent);
+        jdbcTemplate.execute(sqlAddHistoryGrade);
+        jdbcTemplate.execute(sqlAddScienceGrade);
+    }
+
+    @Test
+    public void getStudentsHttpRequest() throws Exception {
+        collegeStudent.setFirstname("Two");
+        collegeStudent.setLastname("Test");
+        collegeStudent.setEmailAddress("one.test@gmail.com");
+        entityManager.persist(collegeStudent);
+        entityManager.flush();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    public void createStudentHttpRequest() throws Exception {
+        collegeStudent.setFirstname("Three");
+        collegeStudent.setLastname("Test");
+        collegeStudent.setEmailAddress("three.test@gmail.com");
+
+        mockMvc.perform(post("/")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(objectMapper.writeValueAsString(collegeStudent)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+
+        CollegeStudent verifyStudent = studentDao.findByEmailAddress("three.test@gmail.com");
+        assertNotNull(verifyStudent, "Student not found");
+    }
+
+    @Test
+    public void deleteStudentHttpRequest() throws Exception {
+        assertTrue(studentDao.findById(1).isPresent());
+        mockMvc.perform(delete("/student/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(0)));
+
+        //assertFalse(studentDao.findById(1).isPresent());
+        Optional<CollegeStudent> verifyStudentOptional = studentDao.findById(1);
+        CollegeStudent verifyStudent = verifyStudentOptional.orElse(null);
+        assertNull(verifyStudent, "Student found");
+
+        verify(studentAndGradeService, times(1)).deleteStudent(1);
+
+        //assertNull(verifyStudent, "Student found");
+    }
+
+
+    @AfterEach
+    public void setAfterTransaction() {
+        jdbcTemplate.execute(sqlDeleteStudent);
+        jdbcTemplate.execute(sqlDeleteMathGrade);
+        jdbcTemplate.execute(sqlDeleteHistoryGrade);
+        jdbcTemplate.execute(sqlDeleteScienceGrade);
+    }
 }
